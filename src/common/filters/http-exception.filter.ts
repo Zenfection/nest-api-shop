7e2b6183481d41044484d2bddb31906644847003
Zenfection <44715845+Zenfection1412@users.noreply.github.com>
2023-06-20 00:42:48 +0700

    fix httpexception filter

--- src/common/filters/http-exception.filter.ts
+++ src/common/filters/http-exception.filter.ts
@@ -3,6 +3,7 @@ import {
   Catch,
   ExceptionFilter,
   HttpException,
+  HttpStatus,
 } from '@nestjs/common';
 import { Response } from 'express';
 
@@ -14,8 +15,14 @@ export class HttpExceptionFilter<T extends HttpException>
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
 
-    const status = exception.getStatus();
-    const exceptionResponse = exception.getResponse();
+    const status =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    const exceptionResponse =
+      exception instanceof HttpException ? exception.getResponse() : exception;
+
     const error =
       typeof response === 'string'
         ? { message: exceptionResponse }
